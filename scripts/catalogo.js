b4ad98c0398f993ddbb9061e926c3b243b8c391a
vmef1980 <99444715+vmef1980@users.noreply.github.com>
2025-10-28 19:05:43 -0600

    Update catalogo.js

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -1,4 +1,4 @@
-import { agregar } from './scripts/pedido.js';
+import { agregar } from 'scripts/pedido.js';
 
 function aplicarFiltroSubcategoria() {
   const seleccionada = document.getElementById("filtroSubcategoria").value;
@@ -129,6 +129,7 @@ document.getElementById("busqueda").addEventListener("input", e => {
     p.style.display = p.textContent.toLowerCase().includes(texto) ? "block" : "none";
   });
 });
+
 
 
 
